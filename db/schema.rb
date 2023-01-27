c00e0404d6b95de3ec1d0afa153543f14c1b63df
Alexander <alexander@MBP-Alexander.netis>
2023-01-27 15:36:50 +0300

    Room created model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_120311) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_123324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_120311) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
